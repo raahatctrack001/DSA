555f3062ab085668b7278bc193db5e744b29f2eb
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-03-03 20:12:16 +0530

    LeetCode 187 Repeated DNA Solution without map

--- Strings/repeatedDNASequence.cpp
+++ Strings/repeatedDNASequence.cpp
@@ -20,3 +20,36 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        if(s.size() <= 10)
+            return {};
+        
+        vector<string> ans;
+       vector<string> patterns;
+        for(int i = 0; i <= s.size()-10; i++)
+            patterns.push_back(s.substr(i, 10));
+        
+        sort(patterns.begin(), patterns.end());
+        for(auto &pattern: patterns)
+            cout<<pattern<<endl;;
+
+        int count = 1;
+        for(int i = 1; i < patterns.size(); i++){
+            if(patterns[i] == patterns[i-1]){
+                count++;
+            }
+            else{
+                if(count > 1)
+                    ans.push_back(patterns[i-1]);
+                count = 1;
+            }
+        }
+        if(count > 1)
+            ans.push_back(patterns.back());
+
+        return ans;      
+    }
+};
