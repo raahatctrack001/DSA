f6d8469576fc8b3960d5e74e70470e03ca857530
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-03-10 19:54:21 +0530

    Leetcode 349 -1 10 March 2024 Intersection of Tow Arrays: solutio without map

--- LeetCode Daily/interSectionOfTwoArrays.cpp
+++ LeetCode Daily/interSectionOfTwoArrays.cpp
@@ -16,3 +16,22 @@ public:
         return ans;
     }
 };
+
+//without map
+class Solution {
+public:
+    vector<int> intersection(vector<int>& first, vector<int>& second) {
+        int freq[1000];
+        for(auto &num: first)
+            freq[num]++;
+        
+        vector<int> ans;
+        for(auto &num: second){
+            if(freq[num] > 0){
+                ans.push_back(num);
+                freq[num] *= -1;
+            }
+        }
+        return ans;
+    }
+};
