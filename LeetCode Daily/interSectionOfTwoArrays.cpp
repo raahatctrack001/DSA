3adfbcc46765e1d36dafc200b5cbaded6859d9e3
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-03-10 19:54:51 +0530

    Leetcode 349 -1 10 March 2024 Intersection of Tow Arrays: solutio with set and binary search

--- LeetCode Daily/interSectionOfTwoArrays.cpp
+++ LeetCode Daily/interSectionOfTwoArrays.cpp
@@ -35,3 +35,36 @@ public:
         return ans;
     }
 };
+
+//set with binary search
+class Solution {
+    auto binarySearch(vector<int> &nums, int key){
+        int start = 0;
+        int end = nums.size()-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == key)
+                return true;
+            else if(nums[mid] < key)
+                start = mid+1;
+            else
+                end = mid-1;
+        }
+        return false;
+    };
+public:
+    vector<int> intersection(vector<int>& first, vector<int>& second) {
+        set<int> st;
+        for(auto &num: first)
+            st.insert(num);
+
+        vector<int> ans;
+        sort(second.begin(), second.end());
+        for(auto &it : st){
+            if(binarySearch(second, it))
+                ans.push_back(it);
+        }
+
+        return ans;
+    }
+};
