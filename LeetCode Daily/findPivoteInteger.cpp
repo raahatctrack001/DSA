6ec7f71bd52802568eeddc594aa5861bd36187c7
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-03-13 12:17:46 +0530

    LeetCode 2485 13 March 2024  Find the Pivote Integer

---
+++ LeetCode Daily/findPivoteInteger.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int preSum = n*(n+1)/2;
+        int currentSum = 0;
+        for(int i = 1; i <= n; i++){
+            currentSum += i;
+            int rightSum = preSum-currentSum+i;
+            if(currentSum == rightSum)
+                return i;
+        }
+
+        return -1;
+    }
+};
