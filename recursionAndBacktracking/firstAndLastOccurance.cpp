8d00a0b6e3fc312e7c589fc5d7a152f378dfad40
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-03-13 12:29:28 +0530

    LeetCode 34 0 Find First and Last Position of Element in Sorted Array

--- recursionAndBacktracking/firstAndLastOccurance.cpp
+++ recursionAndBacktracking/firstAndLastOccurance.cpp
@@ -1,39 +1,5 @@
 class Solution {
-    int firstOccurance(vector<int> &nums, int target){
-        int firstTargetIndex = -1;
-        int start = 0;
-        int end = nums.size()-1;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            if(nums[mid] == target){
-                firstTargetIndex = mid;
-                end = mid-1;
-            }
-            else if(nums[mid] > target)
-                end = mid-1;
-            else 
-                start = mid+1;
-        }
-        return firstTargetIndex;
-    }
 
-    int lastOccurance(vector<int> &nums, int target){
-        int lastTargetIndex = -1;
-        int start = 0;
-        int end = nums.size()-1;
-        while(start <= end){
-            int mid = start + (end-start)/2;
-            if(nums[mid] == target){
-                lastTargetIndex = mid;
-                start = mid+1;
-            }
-            else if(nums[mid]  > target)
-                end = mid-1;
-
-            else start = mid+1;
-        }
-        return lastTargetIndex;
-    }
     int firstOccuranceRecursion(vector<int> &nums, int target, int i = 0){
         if(i >= nums.size())
             return -1;
