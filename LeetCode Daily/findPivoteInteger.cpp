7c64a45c2c367833e8c343d186a0bd12e8596ffe
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-03-13 12:19:23 +0530

    LeetCode 2485 -1  13 March 2024  Find the Pivote Integer : sqrt solution

--- LeetCode Daily/findPivoteInteger.cpp
+++ LeetCode Daily/findPivoteInteger.cpp
@@ -14,3 +14,22 @@ public:
         //
     }
 };
+
+
+//AP Solution .. i.e. x*x = sum
+class Solution {
+public:
+//ap solution: sum(1, n) = sum(1, x) + sum(x, n);
+//sum(1, x) = sum(1, n)-sum(x, n);
+//sum = n*(n+1)/2;
+//=> x*(x+1)/2 = n*(n+1)/2 - (x*(x-1))/2;
+//x*x = n*(n+1)/2;
+
+//based on the same logic
+    int pivotInteger(int n) {
+        int sum = n*(n+1)/2;
+        int pivote = sqrt(sum);
+
+        return pow(pivote, 2) == sum ? pivote : -1;
+    }
+};
