ae4c3663baa7904056de4ec563c497dbd8e13f23
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-03-15 06:14:15 +0530

    LeetCode 238 0 15 March 2024 Product of Array Except Self: Space optimised solution

--- LeetCode Daily/productOfArrayExceptSelf.cpp
+++ LeetCode Daily/productOfArrayExceptSelf.cpp
@@ -17,3 +17,23 @@ public:
         return answer;
     }
 };
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n);
+        prefix[0] = 1;
+        for(int i = 1; i < n; i++)
+            prefix[i] = prefix[i-1]*nums[i-1];
+
+        vector<int> answer(n);
+        int last = 1;
+        for(int i = n-1; i >=0; i--){
+            answer[i] = prefix[i]*last;
+            last *= nums[i];
+        }
+
+        return answer;
+    }
+};
