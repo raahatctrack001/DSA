c89fe467f9aa86dd51d35c5eca203fa1da9cb124
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-03-17 17:13:32 +0530

    LeetCode 57 17 March 2024 Insert Interval in Sorted Intervals: Smooth Code with explaination

--- LeetCode Daily/insertInterval.cpp
+++ LeetCode Daily/insertInterval.cpp
@@ -35,3 +35,45 @@ public:
         return answer;
     }
 };
+
+
+/*
+1. case for including non overlapping intervals before merging i.e. intervals shorter than new Interval
+condition: if(i < n and intervals[i][1] < newIntervals[0])
+
+2. case for handling overlapping intervals: take maximum at end and minimum at start [start, end]
+condition: if(i < n and intervals[i][0] <= newInterval[1]){
+    newInterval[0] = min(newInterval[0], intervals[i][0])
+    newInterval[1] = max(newInterval[1], interval[i][1])
+}
+
+3. include rest of the intervals as no action needed for them:
+*/
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> answer;
+        int i = 0;
+        int n = intervals.size();
+
+        //1. directly include intervals which are not overlapping
+        while(i < n and intervals[i][1] < newInterval[0]){
+            answer.push_back(intervals[i++]);
+        }
+
+        // 2. handle overlapping case
+        while(i < n and intervals[i][0] <= newInterval[1]){
+            newInterval[0] = min(intervals[i][0], newInterval[0]);
+            newInterval[1] = max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        answer.push_back(newInterval);
+
+        // 3. handling non overlapping cases after merging interval
+        while(i < n)
+            answer.push_back(intervals[i++]);
+
+        return answer;
+    }
+};
