830686318025665d2965055c5e03faa97e806073
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-03-25 07:46:21 +0530

    LeetCode 441 0 25 March 2024  Find All Duplicates in an Array: O(n) and O(1) solution

--- LeetCode Daily/findAllDuplicates.cpp
+++ LeetCode Daily/findAllDuplicates.cpp
@@ -11,6 +11,7 @@ public:
     }
 };
 
+//xor solution same values => zero
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
@@ -22,6 +23,23 @@ public:
                 ans.push_back(nums[i]);
             else
                 prev = nums[i];
+        }
+        return ans;
+    }
+};
+
+//O(n) and O(1) solution:
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++){
+            int index = abs(nums[i])-1;
+
+            if(nums[index] < 0)
+                ans.push_back(index+1);
+            
+            nums[index] = -1*nums[index];
         }
         return ans;
     }
