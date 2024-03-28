3fb7be0469cd204d7692311e4ee594c91d6dd779
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-03-28 15:23:52 +0530

    LeetCode 912 0 Sort An Array: In Place Merge Sort :Gap Methodo

--- recursionAndBacktracking/mergeSort.cpp
+++ recursionAndBacktracking/mergeSort.cpp
@@ -38,3 +38,35 @@ public:
         return nums;
     }
 };
+class Solution {
+public:    
+    void merge(vector<int> &nums, int start, int mid, int end){
+        int len = end-start+1;
+        int gap = len/2 + len%2;
+        while(gap > 0){
+            int i = start;
+            int j = start+gap;
+            while(j <= end){
+                if(nums[i] > nums[j])
+                    swap(nums[i], nums[j]);
+
+                i++;
+                j++;
+            }                // gap /= 2;
+            gap = gap == 1 ? 0 : gap/2 + gap%2;
+        }
+    }
+    void mergeSort(vector<int> &nums, int start, int end){
+        if(start >= end)
+            return;
+        int mid = (start+end) >> 1;
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid+1, end);
+        merge(nums, start, mid, end);        
+    }
+    vector<int> sortArray(vector<int>& nums) {
+  
+        mergeSort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
